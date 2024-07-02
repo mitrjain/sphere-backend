f0dc8fc771f67eca2fea2f832e7d4efe964b63d7
Mit Jain <mitjain@Mits-Air.attlocal.net>
2024-07-02 09:36:33 -0700

    Fix network interface binding issue

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python ./create_tables.py
-python manage.py runserver
+fuser -k 8000/tcp
+source sphere-env/bin/activate
+python3 create_tables.py
+python3 manage.py runserver 0.0.0.0:8000
